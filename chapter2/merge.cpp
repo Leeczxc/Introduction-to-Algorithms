9b293631be1743cabb8faa77576061086ed119a0
Leeczxc <leeczxc@outlool.com>
2022-04-15 09:32:50 +0800

    chapter 3

--- chapter2/merge.cpp
+++ chapter2/merge.cpp
@@ -1,4 +1,36 @@
-//
-// Created by Leecz on 2022/4/12.
-//
+#include "iostream"
+#include "vector"
 
+using namespace std;
+
+void merge(vector<int>& array, int start, int mid, int end){
+    auto startIt = array.begin() + start ;
+    vector<int> left(startIt,startIt + ( mid - start) + 1);
+    vector<int> right(startIt + (mid-start) + 1,startIt + ( end - start ) + 1);
+    auto leftIt= left.begin();
+    auto rightIt = right.begin();
+    while(leftIt != left.end() && rightIt != right.end()){
+        if(*leftIt <= *rightIt){
+            *startIt = *leftIt;
+            startIt++;
+            leftIt++;
+        }
+        else{
+            *startIt = *rightIt;
+            startIt++;
+            rightIt++;
+        }
+    }
+    if(leftIt == left.end()){
+        while(rightIt != right.end()){
+            *startIt = *rightIt;
+            rightIt++;
+        }
+    }
+    else{
+        while(leftIt != left.end()){
+            *startIt = *leftIt;
+            leftIt++;
+        }
+    }
+}
--- chapter2/merge_sort.cpp
+++ chapter2/merge_sort.cpp
@@ -1,4 +1,53 @@
-//
-// Created by Leecz on 2022/4/13.
-//
+#include "iostream"
+#include "vector"
 
+using namespace std;
+
+void merge(vector<int>& array, int start, int mid, int end) {
+    auto startIt = array.begin() + start;
+    vector<int> left(startIt, startIt + (mid - start) + 1);
+    vector<int> right(startIt + (mid - start) + 1, startIt + (end - start) + 1);
+    auto leftIt = left.begin();
+    auto rightIt = right.begin();
+    while (leftIt != left.end() && rightIt != right.end()) {
+        if (*leftIt <= *rightIt) {
+            *startIt = *leftIt;
+            startIt++;
+            leftIt++;
+        } else {
+            *startIt = *rightIt;
+            startIt++;
+            rightIt++;
+        }
+    }
+    if (leftIt == left.end()) {
+        while (rightIt != right.end()) {
+            *startIt = *rightIt;
+            rightIt++;
+            startIt++;
+        }
+    } else {
+        while (leftIt != left.end()) {
+            *startIt = *leftIt;
+            leftIt++;
+            startIt++;
+        }
+    }
+}
+
+
+void merge_sort(vector<int>& array, int start, int end){
+    if(start >= end)
+        return ;
+    int mid =  (end + start) / 2;
+    merge_sort(array, start,mid);
+    merge_sort(array, mid + 1, end);
+    merge(array, start, mid, end);
+}
+
+int main(){
+    vector<int> array = {1, 3, 5, 7, 9, 2, 4, 6, 8,10};
+    merge_sort(array,0,9);
+    for(auto c : array)
+        cout << c << " ";
+}
